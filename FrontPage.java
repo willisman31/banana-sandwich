45b50ed0dd54249aeba7cc22bd6ccbed122d21cc
willisman31 <58826505+willisman31@users.noreply.github.com>
2021-03-09 07:27:08 -0500

    Add files via upload

---
+++ FrontPage.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+/**
+ * Front Page class.
+ * @author Willis
+ * Updated 3/9/2021
+ */
+public class FrontPage extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default GUI constructor for Front Page.
+	 */
+	public FrontPage() {
+		setTitle("Music Player");
+	}
+	
+}
